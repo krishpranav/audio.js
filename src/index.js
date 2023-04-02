c224462bcc998f734ad9c8bbf8a8edeb3333d060
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-02 18:33:18 +0530

    index: audio playing units

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 const { exec } = require('child_process')
 const execPromise = require('util').promisify(exec)
 
-const macPlayCommand = (path, volume) => `afplay \"${path}" -v ${volume}`
+const macPlayCommand = (path, volume) => `afplay \"${path}\" -v ${volume}`
 
-const addPresentationCore = `Add-Type -AssemblyName presntationCore;`
+const addPresentationCore = `Add-Type -AssemblyName presentationCore;`
+const createMediaPlayer = `$player = New-Object system.windows.media.mediaplayer;`
+const loadAudioFile = path => `$player.open('${path}');`
+const playAudio = `$player.Play();`
+const stopAudio = `Start-Sleep 1; Start-Sleep -s $player.NaturalDuration.TimeSpan.TotalSeconds;Exit;`
 
+const windowPlayCommand = (path, volume) =>
+  `powershell -c ${addPresentationCore} ${createMediaPlayer} ${loadAudioFile(
+    path,
+  )} $player.Volume = ${volume}; ${playAudio} ${stopAudio}`
 
 module.exports = {
-    play: async (path, volume=0.5) => {
-        const volumeAdjustedByOs = process.platform === 'darwin' ? Math.min(2, volume * 2) : volume
+  play: async (path, volume=0.5) => {
+    const volumeAdjustedByOS = process.platform === 'darwin' ? Math.min(2, volume * 2) : volume
 
-        const playCommand = process.platform === 'darwin' ? macPlayCommand(path, volumeAdjustedByOs)
-        
+    const playCommand =
+      process.platform === 'darwin' ? macPlayCommand(path, volumeAdjustedByOS) : windowPlayCommand(path, volumeAdjustedByOS)
+    try {
+      await execPromise(playCommand, {windowsHide: true})
+    } catch (err) {
+      throw err
     }
+  },
 }
